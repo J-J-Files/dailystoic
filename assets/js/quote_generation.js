f914dc26d89f923d3931f2859a2fdada1ce6bea2
J-J-Files <jophy102@gmail.com>
2024-12-10 11:27:18 -0600

    removed cross file check

--- assets/js/quote_generation.js
+++ assets/js/quote_generation.js
@@ -1,6 +1,19 @@
+/* import { quotes } from './quotes_list.js'; */
 
-import { quotes } from './quotes_list.js';
-
+const quotes = [
+    {
+        quote: "There is no vice which lacks a defense, none that at the outset isn’t modest and easily intervened—but after this the trouble spreads widely. If you allow it to get started you won’t be able to control when it stops. Every emotion is at first weak. Later it rouses itself and gathers strength as it moves along—it’s easier to slow it down than to supplant it.",
+        explanation: "Rivers,” Publilius Syrus reminds us with an epigram, “are easiest to cross at their source.” That’s what Seneca means too. The raging waters and deadly currents of bad habits, ill discipline, chaos, and dysfunction—somewhere they began as no more than just a slight trickle. Somewhere they are a placid lake or pond, even a bubbling underground spring. Which would you rather do—nearly drown in a dangerous crossing in a few weeks or cross now while it’s still easy? It’s up to you."
+    },
+    {
+        quote: "There is no vice which lacks a defense, none that at the outset isn’t modest and easily intervened—but after this the trouble spreads widely. If you allow it to get started you won’t be able to control when it stops. Every emotion is at first weak. Later it rouses itself and gathers strength as it moves along—it’s easier to slow it down than to supplant it.",
+        explanation: "Rivers,” Publilius Syrus reminds us with an epigram, “are easiest to cross at their source.” That’s what Seneca means too. The raging waters and deadly currents of bad habits, ill discipline, chaos, and dysfunction—somewhere they began as no more than just a slight trickle. Somewhere they are a placid lake or pond, even a bubbling underground spring. Which would you rather do—nearly drown in a dangerous crossing in a few weeks or cross now while it’s still easy? It’s up to you."
+    },
+    {
+        quote: "There is no vice which lacks a defense, none that at the outset isn’t modest and easily intervened—but after this the trouble spreads widely. If you allow it to get started you won’t be able to control when it stops. Every emotion is at first weak. Later it rouses itself and gathers strength as it moves along—it’s easier to slow it down than to supplant it.",
+        explanation: "Rivers,” Publilius Syrus reminds us with an epigram, “are easiest to cross at their source.” That’s what Seneca means too. The raging waters and deadly currents of bad habits, ill discipline, chaos, and dysfunction—somewhere they began as no more than just a slight trickle. Somewhere they are a placid lake or pond, even a bubbling underground spring. Which would you rather do—nearly drown in a dangerous crossing in a few weeks or cross now while it’s still easy? It’s up to you."
+    }
+];
 
 function getCurrentQuote() {
     // Determine the day of the year to get a new quote
